afde6faa3e63eb291c7c29b6c1c3110134d3d14d
alfredo-petri <alfredo_petri@hotmail.com>
2024-03-19 15:20:55 -0300

    update usePronouns

--- src/hooks/usePronoun.ts
+++ src/hooks/usePronoun.ts
@@ -1,9 +1,9 @@
 export const usePronoun = () => {
     const pronoun = [
-        { id: "he", value: "Ele/Dele", label: "Ele/Dele" },
-        { id: "she", value: "Ela/Dela", label: "Ela/Dela" },
-        { id: "Other", value: "Outro", label: "Outro" },
-        { id: "no", value: "Prefiro não dizer", label: "Prefiro não dizer" },
+        { value: "he", label: "Ele/Dele" },
+        { value: "she", label: "Ela/Dela" },
+        { value: "Other", label: "Outro" },
+        { value: "no", label: "Prefiro não dizer" },
     ]
     return pronoun
 }
